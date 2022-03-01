36b339bc6d65fa5fe84a8fdd1aba3f5c5ab1aa92
Korsun Pavel <korsun.dev@gmail.com>
2022-03-01 14:00:59 +0300

    Переопределил все методы жизненного цикла фрагмента

--- app/src/main/java/com/shoppinglist/app/presentation/ShopItemFragment.kt
+++ app/src/main/java/com/shoppinglist/app/presentation/ShopItemFragment.kt
@@ -1,8 +1,10 @@
 package com.shoppinglist.app.presentation
 
+import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,22 +18,25 @@ import com.shoppinglist.app.domain.ShopItem
 
 class ShopItemFragment : Fragment() {
 
-
     private lateinit var viewModel: ShopItemViewModel
-
     private lateinit var tilName: TextInputLayout
     private lateinit var tilCount: TextInputLayout
     private lateinit var etName: EditText
     private lateinit var etCount: EditText
     private lateinit var buttonSave: Button
-
     private var screenMode: String = MODE_UNKNOWN
     private var shopItemId: Int = ShopItem.UNDEFINED_ID
 
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.d("message", "onAttach")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         parseParams()
+        Log.d("message", "onCreate")
     }
 
     override fun onCreateView(
@@ -39,6 +44,7 @@ class ShopItemFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Log.d("message", "onCreateView")
         return inflater.inflate(R.layout.fragment_shop_item, container, false)
     }
 
@@ -49,8 +55,43 @@ class ShopItemFragment : Fragment() {
         addTextChangeListeners()
         launchRightMode()
         observeViewModel()
+        Log.d("message", "onViewCreated")
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d("message", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("message", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("message", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("message", "onStop")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d("message", "onDestroyView")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("message", "onDestroy")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.d("message", "onDetach")
+    }
 
     private fun observeViewModel() {
         viewModel.errorInputCount.observe(viewLifecycleOwner) {
